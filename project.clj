84f91884b785a967e4c410d5e6a727044323750e
Nilesh M P-Mac <np@thoughtworks.com>
2011-01-25 14:31:31 +0530

    changed the project files to add ring dependencies.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.6.0-RC1"]]
+                 [compojure "0.6.0-RC1"]
+                 [ring "0.3.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
