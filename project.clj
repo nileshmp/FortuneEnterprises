919edeb56676cf67a0bbdcc3272e04a8399ce935
Nilesh M P <np@thoughtworks.com>
2011-03-04 17:54:32 +0530

    Start working on DB....trying out sample examples with clojureql.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject FortuneEnterprises "1.0.0-SNAPSHOT"
-  :main FortuneEnterprises.startserver
+  :main FortuneEnterprises.DB.ConnectToH2
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
--- src/FortuneEnterprises/DB/ConnectToH2.clj
+++ src/FortuneEnterprises/DB/ConnectToH2.clj
@@ -1,6 +1,15 @@
+(ns FortuneEnterprises.DB.ConnectToH2
+ (:use clojure.contrib.sql))
+
+;
+; specify the path to your database driver
+; 
+;; (add-classpath "file:///~/Work/h2/bin/h2-1.3.151.jar")
+
+
 (let [db-protocol "tcp"            ; "file|mem|tcp"
       db-host     "192.168.1.2:8082" ; "path|host:port"
-      db-name     "FE"]
+      db-name     "~/FE"]
  
   (def db {:classname   "org.h2.Driver" ; must be in classpath
            :subprotocol "h2"
@@ -10,16 +19,19 @@
            :user     "fortune"
            :password "fortune"}))
  
-;
-; specify the path to your database driver
-; 
-(add-classpath "file:///c:/Installation/h2/bin/h2.jar")
  
 ;;
 ;; Here is an example of creating a symbol in the 
 ;; existing namespace as an alias to a namespace
 ;;
-;(require '[clojure.contrib.sql :as sql]) 
-;(sql/with-connection db
-;  (sql/with-query-results rs ["select * from customer"]
-;    (dorun (map #(println (:lastname %)) rs))))
+(require '[clojure.contrib.sql :as sql])
+
+(defn execute-sql []
+  (println "inside execute-sql")
+  (sql/with-connection db
+    (sql/with-query-results rs ["select * from MERCHANT"]
+      (dorun (map #(println (:NAME %)) rs)))))
+
+(defn -main [& args]
+  (println "inside main")
+  (execute-sql))
